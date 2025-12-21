a55b022ddb4ffd78a18b879d8d8dcf50edd96932
faizan99904 <faizan99904@gmail.com>
2025-12-21 22:36:51 +0500

    header layout tab-nav done

--- src/app/shared/header/header.component.ts
+++ src/app/shared/header/header.component.ts
@@ -1,11 +1,12 @@
-import { Component } from '@angular/core';
-
+import { Component, HostListener } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 @Component({
   selector: 'app-header',
-  imports: [],
+  imports: [FormsModule, CommonModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
 export class HeaderComponent {
-
+  
 }
